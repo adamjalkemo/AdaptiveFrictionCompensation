226ec9559c84f93bf31e4c65235f61e4d461314e
mat11ja1 <jonte.a89@hotmail.com>
2016-04-05 12:26:07 +0200

    Added files via upload
    blandat skit

---
+++ matlab/FrictionEstim.m
@@ -0,0 +1,3 @@
+uF = [mean(um30) mean(um25) mean(um20) mean(um15) mean(um10) mean(um05) 0 mean(u05) mean(u10) mean(u15) mean(u20) mean(u25) mean(u30)];
+uR = [-3 -2.5 -2 -1.5 -1 -0.5 0 0.5 1 1.5 2 2.5 3];
+plot(uR,uF,'o')
