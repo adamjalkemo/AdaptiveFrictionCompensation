ff0749dc8e23bf49c32cea19c4258e1a6e65a605
Adam Jalkemo <adam@jalkemo.se>
2016-04-13 19:06:14 +0200

    Added java implementation of C2D

--- Furuta-Controller/src/main/java/com/aaej/Dlqr.java
+++ Furuta-Controller/src/main/java/com/aaej/Dlqr.java
@@ -48,8 +48,38 @@ class Dlqr {
 		return L;
 	}
 
+	// Returns Matrix vector [Matrix Ad, Matrix Bd]
+	public static Matrix[] c2d(Matrix Ac, Matrix Bc, long h) {
+		/*% Approximations for Ad = expm(Ac*h)
+		    %ZOH (if expm available)
+		    %expm(Ac*h)
+
+		    %eye(size(Ac)) + Ac*h (Worst)
+
+		    %inv(eye(size(Ac)) - Ac*h) (Medium good)
+
+		    %Bilinear transform (Best wo. exmp function)
+		    (eye(size(Ac))+1/2*Ac*h)*inv(eye(size(Ac))-1/2*Ac*h)
+		    
+		% Approximations for Bd
+		    % Medium (works)
+		    h * Bc
+
+		    % Probably better, but wont work due to Ac not invertible.
+		    %inv(Ac)*(Ad - eye(size(Ad)))*Bc*/
+
+		    int m = Ac.getRowDimension();
+		    // h converted to seconds below!
+		    Matrix Ad = Matrix.identity(m,m).plus(Ac.times(0.5*h/1000)).times((Matrix.identity(m,m).minus(Ac.times(0.5*h/1000))).inverse());
+		    Matrix Bd = Bc.times((double) h / 1000);
+		    System.out.println(Arrays.deepToString(Ad.getArray()));
+		    System.out.println(Arrays.deepToString(Bd.getArray()));
+		    return new Matrix[]{Ad, Bd};
+	}
+
 	// I'm leaving this here for manual test purposes
-	public static void main(String[] args) { 
+	/*public static void main(String[] args) { 
+		
 		Matrix A = new Matrix(new double[][]{{-2, 0},{0, -1}});
 		
 		Matrix B = new Matrix(new double[]{1, 0.1},2);
@@ -58,6 +88,13 @@ class Dlqr {
 		Matrix R = new Matrix(new double[]{1},1);
 
 		System.out.println(Arrays.deepToString(Dlqr.getL(A,B,Q,R).getArray()));
-	}
+		
+		Matrix Ac = new Matrix(new double[][]{{0, 1, 0, 0},{31.3167200666837, 0, 0, 0},{0, 0, 0, 1,},{-0.588392468761969, 0, 0, 0}});
+		Matrix Bc = new Matrix(new double[]{0, -71.2339550559284, 0 ,191.245792952122},4);
+		c2d(Ac,Bc,10);
+		
+
+	}*/
 
 }
+	
