ebb61216b6b3938f7eb8c92d761e251fb8a29483
Adam Jalkemo <adam@jalkemo.se>
2016-04-14 13:57:55 +0200

    Fixed a parentesis in the code

--- Furuta-Controller/src/main/java/com/aaej/SwingUpController.java
+++ Furuta-Controller/src/main/java/com/aaej/SwingUpController.java
@@ -13,7 +13,8 @@ class SwingUpController {
     public synchronized double calculateOutput(double pendAng, double pendAngVel) {
         double omegaSquared = controllerParameters.omega0 * controllerParameters.omega0;
         double pendAngVelSquared = pendAngVel*pendAngVel;
-        double y = (cos(pendAng) * (pendAngVel * (cos(pendAng)) - 1 + pendAngVelSquared / (2 * omegaSquared)));
+        //double y = (cos(pendAng) * (pendAngVel * (cos(pendAng)) - 1 + pendAngVelSquared / (2 * omegaSquared)));
+        double y = cos(pendAng) * pendAngVel * (cos(pendAng) - 1 + pendAngVelSquared / (2 * omegaSquared));
         return controllerParameters.gain*signum(y);
     }
 
